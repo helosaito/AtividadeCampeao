39af3d09f22bb929ff275fea1d77c8fd21b7e37e
unknown <helo.saito@outlook.com>
2026-06-03 19:52:42 -0300

    criado

---
+++ src/test/java/com/Campeao/atividade/AtividadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Campeao.atividade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
